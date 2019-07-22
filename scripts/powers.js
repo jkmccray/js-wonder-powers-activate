c4afa596c6757740ea787fcd860a0f7e1f3e9c88
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-21 21:32:42 -0500

    Completed practice 'wonder powers activate'

--- scripts/powers.js
+++ scripts/powers.js
@@ -1,7 +1,37 @@
+// Flight Button
 const flightHandlerFunction = () => {
   document.querySelector("#flight").classList.toggle("disabled")
   document.querySelector("#flight").classList.toggle("enabled")
 }
 
-document.querySelector("#activate-flight").addEventListener("click",flightHandlerFunction)
+document.querySelector("#activate-flight").addEventListener("click", flightHandlerFunction)
+
+// Mind Reading Button
+const mindReadingHandlerFunction = () => {
+  document.querySelector("#mindreading").classList.toggle("disabled")
+  document.querySelector("#mindreading").classList.toggle("enabled")
+}
+
+document.querySelector("#activate-mindreading").addEventListener("click", mindReadingHandlerFunction)
+
+// Xray Button
+const xrayHandlerFunction = () => {
+  document.querySelector("#xray").classList.toggle("disabled")
+  document.querySelector("#xray").classList.toggle("enabled")
+}
+
+document.querySelector("#activate-xray").addEventListener("click", xrayHandlerFunction)
+
+// Activate All and Deactivate All
+const collectionOfSections = document.querySelectorAll(".power")
+const allEventHandlerFunction = () => {
+  collectionOfSections.forEach(section => {
+    section.classList.toggle("disabled")
+    section.classList.toggle("enabled")
+  })
+}
+
+document.querySelector("#activate-all").addEventListener("click", allEventHandlerFunction)
+
+document.querySelector("#deactivate-all").addEventListener("click", allEventHandlerFunction) 
 
